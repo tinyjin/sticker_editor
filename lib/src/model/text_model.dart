cfaf8f24305e4f4ebd40b768015178eb15472753
Jinny You <baram991103@gmail.com>
2022-12-13 19:19:25 +0900

    Add json model for TextModel

--- lib/src/model/text_model.dart
+++ lib/src/model/text_model.dart
@@ -17,4 +17,24 @@ class TextModel {
       required this.textAlign,
       required this.scale,
       required this.left});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': name,
+      'top': top,
+      'left': left,
+      'scale': scale,
+      'textStyle': {
+        'font': textStyle.fontFamily,
+        'fontStyle': textStyle.fontStyle?.index ?? 0,
+        'fontWeight': textStyle.fontWeight?.index ?? 0,
+        'fontSize': textStyle.fontSize,
+        'height': textStyle.height,
+        'letterSpacing': textStyle.letterSpacing,
+        'color': textStyle.color?.value ?? '',
+        'backgroundColor': textStyle.backgroundColor?.value ?? '',
+      },
+      'textAlign': textAlign.index
+    };
+  }
 }
